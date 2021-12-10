9bd20c890fda542c2c63b9920a92ed9a1d5134e8
Christopher Seidl <KeilChris@users.noreply.github.com>
2021-12-10 15:07:51 +0100

    Initial push

---
+++ RTE/_NUCLEO-G474RE/RTE_Components.h
@@ -0,0 +1,35 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Blinky' 
+ * Target:  'NUCLEO-G474RE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g4xx.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.3 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/* Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/* Keil.STM32CubeMX::Device:STM32Cube Framework:STM32CubeMX:1.4.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+/* Keil.STM32CubeMX::Device:STM32Cube HAL:1.4.0 */
+#define RTE_DEVICE_CUBE_MX_HAL
+/* Keil::CMSIS Driver:VIO:Board:NUCLEO-G474RE:1.0.0 */
+#define RTE_VIO_BOARD
+        #define RTE_VIO_NUCLEO_G474RE
+
+
+#endif /* RTE_COMPONENTS_H */
